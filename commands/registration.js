e67028cc6d78d55a24f360fe125679f921f14869
Sergey Belov <sergeytrenerbelov@gmail.com>
2024-03-25 12:14:27 +0200

    саа

--- commands/registration.js
+++ commands/registration.js
@@ -1,6 +1,13 @@
 const { checkUserName } = require("../helpers/helpers");
 const { apiService } = require("../apiService/apiService");
 //Here...
+
+const registrationQuestions = [
+  "Введите имя ",
+  "Введите фамилию ",
+  "Введите рост",
+];
+
 const registrationOfNewUserComand = async (ctx) => {
   const userName = checkUserName(ctx);
   try {
