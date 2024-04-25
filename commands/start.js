60bf88d3f3bacf1611be311a09b49c915cac1705
Sergey Belov <sergeytrenerbelov@gmail.com>
2024-04-25 16:48:16 +0300

    sd

--- commands/start.js
+++ commands/start.js
@@ -4,28 +4,48 @@ const { User } = require("../user/user");
 const userState = require("../userState/userState");
 
 const regAnswers = { name: null, email: null, phone: null };
-
+//Here...
 const startCommand = async (ctx) => {
   const userName = botHelper.checkUserName(ctx);
-
+  const currentUser = userState.findUser(userName);
   try {
     const { isExist } = await apiService.findUser(userName);
-    isExist
-      ? userState.addNewUser(
-          new User(userName, botHelper.getQuestionTitlesForNewSet())
-        )
-      : userState.addNewUser(
-          new User(userName, botHelper.getRegistrationQuestions())
-        );
 
-    const currentUser = userState.findUser(userName);
-    currentUser.setUnswers(regAnswers);
-    await botHelper.markupReplier(
-      ctx,
-      "Выберите опцию :",
-      botHelper.getstartOptions(),
-      "typeOfAction"
-    );
+    if (isExist && !currentUser) {
+      userState.addNewUser(
+        new User(userName, botHelper.getQuestionTitlesForNewSet())
+      );
+      return await botHelper.markupReplier(
+        ctx,
+        "Выберите опцию :",
+        botHelper
+          .getstartOptions()
+          .slice(1, botHelper.getstartOptions().length),
+        "typeOfAction"
+      );
+    }
+    if (!isExist) {
+      userState.addNewUser(
+        new User(userName, botHelper.getRegistrationQuestions())
+      );
+      const currentUser = userState.findUser(userName);
+      currentUser.setUnswers(regAnswers);
+      return await botHelper.markupReplier(
+        ctx,
+        "Выберите опцию :",
+        botHelper.getstartOptions(),
+        "typeOfAction"
+      );
+    } else {
+      return await botHelper.markupReplier(
+        ctx,
+        "Выберите опцию :",
+        botHelper
+          .getstartOptions()
+          .slice(1, botHelper.getstartOptions().length),
+        "typeOfAction"
+      );
+    }
   } catch (error) {
     console.log(error.message);
     console.log("error in startComand");
